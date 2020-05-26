1feea3fe0a9f34fd1b9a376051cbc0b25ff59dec
Eamonn Smyth <e.smyth75@gmail.com>
2020-05-26 22:05:08 +0000

    Commented out previous lesson to begin next lesson

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,8 +1,8 @@
-function addition(numOne, numTwo){
-    if(typeof(numOne) == "number" && typeof(numTwo) == "number"){ /* Known as defensive programming - checks for unwanted entries */
-        return numOne + numTwo;
+/* function addition(numOne, numTwo){
+    if(typeof(numOne) == "number" && typeof(numTwo) == "number"){ */ /* Known as defensive programming - checks for unwanted entries */
+/*         return numOne + numTwo;
     } else {
         alert("Error");
     }
     
-}
+} */
