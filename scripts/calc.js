75242341cfb6ca9f8b65ac5e31fe96959b19d186
Eamonn Smyth <e.smyth75@gmail.com>
2020-05-27 20:38:49 +0000

    Add new files to start drink testing

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,27 @@
+/* SECOND LESSON ON TESTING */
+
+/* CALCULATOR OBJECT TO ADD ONE NUMBER */
+Calculator = function() {
+    this.value = 0;
+}
+
+/* ADDING THE ADDITION FUNCTION TO THE cALCULATOR OBJECT */
+Calculator.prototype.add = function(number) {
+    if (typeof(number) == "number"){
+        this.value += number;
+    } else {
+        alert("Error!");
+    }
+}
+
+
+
+
+
+
+
+/* FIRST LESSON ON TESTING */
+
 /* function addition(numOne, numTwo){
     if(typeof(numOne) == "number" && typeof(numTwo) == "number"){ */ /* Known as defensive programming - checks for unwanted entries */
 /*         return numOne + numTwo;
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,4 +1,46 @@
+/* SECOND LESSON ON TESTING */
+/* MODIFY PREVIOUS CODE TO MATCH NEW FUNCTION IN CALC.JS */
 describe("Calculator", function() {
+
+    beforeEach(function(){ /* THIS BeforeEach PREVENTS THE TOTAL RUNNING INTO NEXT TEST */
+        calc = new Calculator; /* CREATE NEW INSTANCE OF CALCULATOR OBJECT*/
+    });
+    
+
+    describe("Addition tests", function() {
+        it("should return 42", function() {
+            calc.add(20);
+            calc.add(22);
+            expect(calc.value).toBe(42);
+        });
+
+        it("should return 26", function() {
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26);
+        });
+
+        it("should return an error if we don't supply two numbers", function() {
+        spyOn(window, "alert");
+        calc.add("Hitchhikers");
+        expect(window.alert).toHaveBeenCalledWith("Error!");
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+/* FIRST LESSON ON TESTING */
+/* describe("Calculator", function() {
+
+    let calc = new Calculator;
+
     describe("Addition tests", function() {
         it("should return 42", function() {
             expect(addition(20, 22)).toBe(42);
@@ -12,4 +54,4 @@ describe("Calculator", function() {
         expect(window.alert).toHaveBeenCalledWith("Error");
         });
     });
-});
+}); */
